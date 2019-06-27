1149fcfb47aa5f971dbc2c7338378654dba2cc92
Justin TM <ustjin86@gmail.com>
2019-06-26 20:03:11 -0400

    Updates

---
+++ precache-manifest.2b21c30697f4d789387dfdf5f4828803.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02a77a5d97f55b1b8c767e80e5c5d7a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbc22497241afdcf8f68",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "6a4609bfe0ac0779eba3",
+    "url": "/static/css/main.f31d6ac8.chunk.css"
+  },
+  {
+    "revision": "bbc22497241afdcf8f68",
+    "url": "/static/js/2.3fe3dce6.chunk.js"
+  },
+  {
+    "revision": "6a4609bfe0ac0779eba3",
+    "url": "/static/js/main.6b3cf56d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "35d6f0f779865b48eff73b3b8970b1f8",
+    "url": "/static/media/logo_actionable_data.35d6f0f7.png"
+  },
+  {
+    "revision": "caddfc0001548e9397c5352ee4e807ae",
+    "url": "/static/media/logo_aws.caddfc00.jpg"
+  },
+  {
+    "revision": "59805dca50d76f6c406b5fa5c2c17e14",
+    "url": "/static/media/logo_cloudfoundry.59805dca.png"
+  },
+  {
+    "revision": "93e453005b2fce158c6cf7adab283cbd",
+    "url": "/static/media/logo_datafestival.93e45300.jpg"
+  },
+  {
+    "revision": "bdfefca9a1fe65d33e2b355ed89d98ab",
+    "url": "/static/media/logo_datanatives.bdfefca9.jpg"
+  },
+  {
+    "revision": "8568b7a92e21b78452d2427bf1d21196",
+    "url": "/static/media/logo_freecodecamp.8568b7a9.png"
+  },
+  {
+    "revision": "674168aa204792ee69e6b2fd2ba953a0",
+    "url": "/static/media/logo_hub.674168aa.jpg"
+  },
+  {
+    "revision": "5fcfbe5fab8888143de265d25632f53f",
+    "url": "/static/media/logo_idealotech.5fcfbe5f.jpg"
+  },
+  {
+    "revision": "dca786f82425dca13a6d49a8e9575000",
+    "url": "/static/media/logo_meetup.dca786f8.png"
+  },
+  {
+    "revision": "61de0b9afe362548b44d4bfc0dbdf58b",
+    "url": "/static/media/logo_nvidia.61de0b9a.png"
+  },
+  {
+    "revision": "0ad1bf7e24790963fa357262bd817e70",
+    "url": "/static/media/logo_pydata.0ad1bf7e.png"
+  },
+  {
+    "revision": "413fcdf275743ec4a66bed1c617005f5",
+    "url": "/static/media/logo_towards_data_science.413fcdf2.jpg"
+  },
+  {
+    "revision": "f8e99ef4120466bdc81c8b5b00e14a04",
+    "url": "/static/media/logo_venturi.f8e99ef4.jpg"
+  }
+]);
